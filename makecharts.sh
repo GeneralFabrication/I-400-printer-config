0edf0f015ef3d91f7833f4d1a9420d759a4e9116
SteveGenFab <159185798+SteveGenFab@users.noreply.github.com>
2024-06-04 12:23:54 -0400

    Revert "Merge branch 'main' of https://github.com/GeneralFabrication/I-400-printer-config"
    This reverts commit 2bcc24d7748dcde17314ad1d52c4a9026ded1407, reversing
    changes made to ddde9c55d050382f489ef27de7254c79dab6238a.

--- makecharts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo calibrating for X axis:
-~/klipper/scripts/calibrate_shaper.py /tmp/calibration_data_x_*.csv -o ~/printer_data/config/charts/shaper_calibrate_x_$(date "+%F_%H:%M").png
-echo
-echo calibrating for y axis:
-~/klipper/scripts/calibrate_shaper.py /tmp/calibration_data_y_*.csv -o ~/printer_data/config/charts/shaper_calibrate_y_$(date "+%F_%H:%M").png
-rm /tmp/calibration_data_x_*.csv /tmp/calibration_data_y_*.csv
